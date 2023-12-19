e1c9947db428aa5a53cf1ed9d12862765bac94c3
Lucas Bezerra Marques de Souza <lucasbezerra527@gmail.com>
2023-12-19 14:24:16 -0300

    Update meanshift.py

--- meanshift.py
+++ meanshift.py
@@ -8,16 +8,16 @@ from sklearn.cluster import estimate_bandwidth, MeanShift
 import numpy as np
 from tqdm import tqdm
 
-def aplicaMeanShiftOneImg(img):
-    img_suavizada = gaussian(img, sigma=1)
+def aplicaMeanShiftOneImg(img, quantile=0.2, n_samples=500, clip_limit=0.03, default_bandwith=0.1, sigma=1):
+    img_suavizada = gaussian(img, sigma)
 
     img_gray = rgb2gray(img_suavizada)
 
-    img_eq = exposure.equalize_adapthist(img_gray, clip_limit=0.03)
+    img_eq = exposure.equalize_adapthist(img_gray, clip_limit=clip_limit)
 
     # Estimativa do bandwidth para o Mean Shif
     flat_img = img_eq.reshape((-1, 1))
-    bandwidth = max(estimate_bandwidth(flat_img, quantile=0.2, n_samples=500), 0.1)
+    bandwidth = max(estimate_bandwidth(flat_img, quantile=quantile, n_samples=n_samples), default_bandwith)
 
     # Mean Shift
     ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
@@ -33,10 +33,10 @@ def aplicaMeanShiftOneImg(img):
     return cleaned
 
 
-def aplicar_menshift_paralelo(list_img):
+def aplicar_menshift_paralelo(list_img, **kwargs):
     with Pool(processes=16) as pool:
         result_list = list(
-            tqdm(pool.imap(aplicaMeanShiftOneImg, list_img), total=len(list_img), desc="Processando Mean Shift"))
+            tqdm(pool.imap(aplicaMeanShiftOneImg, list_img, **kwargs), total=len(list_img), desc="Processando Mean Shift"))
     return result_list
 
 
